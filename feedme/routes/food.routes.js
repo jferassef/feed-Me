e0adfcfd39f697a3a7406802a5c9e575b0b81f98
Jessica Fernandez <jferasef@gmail.com>
2022-05-27 22:33:27 +0200

    routes and views for food

--- feedme/routes/food.routes.js
+++ feedme/routes/food.routes.js
@@ -18,3 +18,65 @@ router.get("/", async (req, res, next) => {
 router.get("/foods/food-create", (req, res, next) => {
   res.render("foods/food-create");
 });
+
+router.post("/foods/food-create", async (req, res, next) => {
+  try {
+    const { name, category, imageUrl, expireDate, quantity, note } = req.body;
+    await Food.create({
+      name,
+      category,
+      imageUrl,
+      expireDate,
+      quantity,
+      note,
+    });
+
+    res.redirect("/foods");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("foods/:id/edit", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const food = await Food.findById(id);
+    res.render("foods/food-edit", food);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/:id/edit", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, category, imageUrl, expireDate, quantity, note } = req.body;
+    await Food.findByIdAndUpdate(
+      id,
+      { name, category, imageUrl, expireDate, quantity, note },
+      { new: true }
+    );
+    res.redirect("/foods");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/:id/delete", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Food.findByIdAndDelete(id);
+    res.redirect("/foods");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const foodDetails = await Food.findById(req.params.id);
+    res.render("rooms/room", foodDetails);
+  } catch (error) {
+    console.log(error);
+  }
+});
