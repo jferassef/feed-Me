5f04f54d0e0c51fdfd81f5d941eab0126026a3f9
cristina <cristinahurtadocm@gmail.com>
2022-05-28 13:49:37 +0200

    home, views, home post

--- feedme/public/js/script.js
+++ feedme/public/js/script.js
@@ -5,3 +5,15 @@ document.addEventListener(
   },
   false
 );
+
+
+async function welcomeView(){
+  const imgIcon = document.querySelector(".icon-app");
+await imgIcon(0);
+const welcomeTxt = Document.querySelector(".welcome>h1");
+await welcomeTxt(1);
+const name = Document.querySelector(".welcome p");
+await name(2);
+}
+
+welcomeView();
--- feedme/routes/index.routes.js
+++ feedme/routes/index.routes.js
@@ -1,9 +1,25 @@
 const router = require("express").Router();
+const User = require("../models/User.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.post("/", async(req, res, next)=>{
+  try {
+    const { name} = req.body;
+    await User.create({
+      name
+    });
+    res.redirect("auth/signup");
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+
 
 router.use("/foods", require("./foods.routes"));
 
