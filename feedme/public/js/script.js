d4c7519d377c920b82684332df07aba38ea08f69
Jessica Fernandez <jferasef@gmail.com>
2022-06-04 11:37:32 +0200

    shop-list finished styles missing

--- feedme/public/js/script.js
+++ feedme/public/js/script.js
@@ -16,12 +16,28 @@ await name(2);
 }
 welcomeView();*/
 
-function alertMessage() {
+/* function alertMessage() {
   const total = document.querySelector(".total");
-  console.log(total.innerHTML);
   if (total.innerHTML < 2) {
     alert("Please go buy food");
   }
-}
+} 
 
-alertMessage();
+alertMessage(); */
+
+const itemContainer = document.querySelector(".itemContainer");
+const addNewItem = (event) => {
+  event.preventDefault();
+  const { value } = event.target.itemText;
+  if (!value) return;
+  const item = document.createElement("li");
+  item.addEventListener("click", changeItemState);
+  item.textContent = value;
+  itemContainer.prepend(item);
+  console.log(itemContainer.prepend(item));
+  event.target.reset();
+};
+
+changeItemState = (event) => {
+  event.target.classList.toggle("done");
+};
