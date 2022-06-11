8b70a4c7ec8554dbede3f99f345919c8736b3b05
Jessica Fernandez <jferasef@gmail.com>
2022-06-11 03:46:58 +0200

    final touches

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,16 +6,4 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { name } = req.body;
-    await User.create({
-      name,
-    });
-    res.redirect("auth/signup");
-  } catch (error) {
-    next(error);
-  }
-});
-
 module.exports = router;
